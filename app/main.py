3c548c0cec4f44f6b887f06af25b1a001aca1a0b
Gilderlan silva da cruz <lansilva007@gmail.com>
2024-10-08 21:30:33 -0300

    Kivy

---
+++ app/main.py
@@ -0,0 +1,80 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+)
+from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
+
+# Define a bola do jogo, com propriedades para a velocidade
+# e metodo para mover a bola.
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+#A classe principal do jogo, que gerencia a lógica do jogo,
+# incluindo o movimento da bola, a verificação de colisões e o gerenciamento da pontuação.
+class PongGame(Widget):
+    ball = ObjectProperty(None)  # bola
+    player1 = ObjectProperty(None) # jogador 1
+    player2 = ObjectProperty(None) # jogador 2
+
+    def serve_ball(self, vel=(4, 0)):
+        self.ball.center = self.center  # bola nascer no centro da tela
+        self.ball.velocity = Vector(*vel).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce off paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+        # bounce ball off bottom or top
+        if (self.ball.y < self.y) or (self.ball.top > self.top):
+            self.ball.velocity_y *= -1
+
+        # went off to a side to score point?
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(4, 0))
+        if self.ball.right > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(-4, 0))
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width / 3:
+            self.player2.center_y = touch.y
+
+
+class PongApp(App):
+    def build(self):
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
+
+# Representa as raquetes dos jogadores e possui um método
+# para fazer a bola quicar ao colidir com a raquete
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
+
+
+if __name__ == '__main__':
+    PongApp().run()
